2a1604f9e5cec7ab3952364a7df0f4c3212ed1cc
Evheniy Bystrov <evheniy.bystrov@gmail.com>
2017-07-14 10:41:59 +0300

    single redis instance

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const debug = require('debug')('koa2:ioredis');
-const Redis = require('ioredis');
-const config = require('config');
-
-const redis = new Redis(config.redis);
+const redis = require('./redis');
 
 module.exports = () => async (ctx, next) => {
   ctx.redis = redis;
-  debug('ctx.redis');
+  debug('Redis created');
   await next();
 };
